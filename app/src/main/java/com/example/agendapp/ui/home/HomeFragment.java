446f156bf265865b3c30a8306c09f4d996054b72
AlexanderEstebanZapata1994 <alexanderesteban7@gmail.com>
2020-01-29 22:39:35 -0500

    Se ha añadido la funcionalidad para guardar en las preferencias compartidas de la app

--- app/src/main/java/com/example/agendapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/agendapp/ui/home/HomeFragment.java
@@ -1,23 +1,37 @@
 package com.example.agendapp.ui.home;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
 import com.example.agendapp.R;
+import com.google.android.material.snackbar.Snackbar;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private EditText editTextNombre;
+    private EditText editTextDatos;
+
+    private Button btnGuardar;
+    private Button btnBuscar;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
+                             ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
@@ -28,6 +42,56 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        editTextNombre = (EditText) root.findViewById(R.id.txt_Nombre);
+        editTextDatos = (EditText) root.findViewById(R.id.txt_Datos);
+
+        btnBuscar = (Button)root.findViewById(R.id.btnBuscar);
+
+        btnBuscar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Buscar(v);
+            }
+        });
+        btnGuardar = (Button) root.findViewById(R.id.btnGuardar);
+
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Guardar(v);
+            }
+        });
         return root;
+    }
+
+    //Método para guardar
+    public void Guardar(View view){
+        String nombre = editTextNombre.getText().toString();
+        String datos = editTextDatos.getText().toString();
+
+        SharedPreferences preferences = this.getActivity().getSharedPreferences("agenda", Context.MODE_PRIVATE);
+        SharedPreferences.Editor objEditor = preferences.edit();
+        objEditor.putString(nombre, datos);
+        objEditor.commit();
+
+        Snackbar.make(getActivity().findViewById(R.id.fragment_home), "La información ha sido guardada correctamente.", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+
+        //Toast.makeText(this.getActivity(), "La información ha sido guardada correctamente.", Toast.LENGTH_LONG).show();
+    }
+
+    //Metodo para buscar
+    public void Buscar(View view){
+        String nombre = editTextNombre.getText().toString();
+
+        SharedPreferences preferences = this.getActivity().getSharedPreferences("agenda", Context.MODE_PRIVATE);
+        String datos = preferences.getString(nombre, "");
+
+        if (datos.length() == 0){
+            Toast.makeText(this.getActivity(), "No se encontró ningún registro.", Toast.LENGTH_LONG).show();
+        }else {
+            editTextDatos.setText(datos);
+        }
     }
 }
